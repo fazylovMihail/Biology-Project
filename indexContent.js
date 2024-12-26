93be0f6b9204a983d97b18b77160eb1219c01ee3
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2024-12-26 22:22:23 +0500

    стили для демонстративного показа (рапорта)

--- indexContent.js
+++ indexContent.js
@@ -1,5 +1,6 @@
 const h1 = document.querySelectorAll('h1');
 
 for(let i = 0;i<h1.length;i++){
-    h1[i].innerHTML = localStorage.getItem('name')
+    h1[0].innerHTML = localStorage.getItem('name');
+    h1[1].innerHTML = localStorage.getItem('description');
 }
--- js/arrays.js
+++ js/arrays.js
@@ -1,4 +1,4 @@
 let names = ['dfdfddf','xvxvcvc','ererrere','sdsdsd',';lklklklk','cvcnhhyuyuuyuy'];
-let descriptions = ['Билиберда','Билиберда','Билиберда','Билиберда','Билиберда','Билиберда'];
+let descriptions = ['Билиберда1','Билиберда2','Билиберда3','Билиберда4','Билиберда5','Билиберда6'];
 
 export {names, descriptions}
